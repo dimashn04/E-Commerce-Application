9c9b644e64b79ebf53b23e218a5ba524dcd60f8b
dimashn04 <dimashn04@gmail.com>
2025-02-12 22:19:20 +0700

    Added BankTransferDTO

---
+++ ECommerceApplication/src/main/java/com/app/payloads/BankTransferDTO.java
@@ -0,0 +1,15 @@
+package com.app.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankTransferDTO {
+    
+    private String bankName;
+    private String cardNumber;
+
+}
