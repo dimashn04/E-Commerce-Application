7297a37adfa366baa46d7bf5aabd9ced21003442
dimashn04 <dimashn04@gmail.com>
2025-02-13 19:57:46 +0700

    Fixed to parse BankTransferDTO instead

--- ECommerceApplication/src/main/java/com/app/services/OrderService.java
+++ ECommerceApplication/src/main/java/com/app/services/OrderService.java
@@ -2,12 +2,13 @@ package com.app.services;
 
 import java.util.List;
 
+import com.app.payloads.BankTransferDTO;
 import com.app.payloads.OrderDTO;
 import com.app.payloads.OrderResponse;
 
 public interface OrderService {
 	
-	OrderDTO placeOrder(String email, Long cartId, String paymentMethod, String bankName);
+	OrderDTO placeOrder(String email, Long cartId, String paymentMethod, BankTransferDTO bankTransferDTO);
 	
 	OrderDTO getOrder(String email, Long orderId);
 	
