7ae1376f3163e43af1f134d601633548131889bd
dimashn04 <dimashn04@gmail.com>
2025-02-13 19:58:13 +0700

    Fixed RequestBody import

--- ECommerceApplication/src/main/java/com/app/controllers/OrderController.java
+++ ECommerceApplication/src/main/java/com/app/controllers/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.config.AppConstants;
@@ -19,7 +20,6 @@ import com.app.payloads.OrderDTO;
 import com.app.payloads.OrderResponse;
 import com.app.services.OrderService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
@@ -32,8 +32,13 @@ public class OrderController {
 	public OrderService orderService;
 	
 	@PostMapping("/public/users/{email}/carts/{cartId}/payments/{paymentMethod}/order")
-	public ResponseEntity<OrderDTO> orderProducts(@PathVariable String email, @PathVariable Long cartId, @PathVariable String paymentMethod, @Valid @RequestBody BankTransferDTO bankTransferDTO) {
-		OrderDTO order = orderService.placeOrder(email, cartId, paymentMethod, bankTransferDTO.getBankName());
+	public ResponseEntity<OrderDTO> orderProducts(
+		@PathVariable String email, 
+		@PathVariable Long cartId, 
+		@PathVariable String paymentMethod, 
+		@Valid @RequestBody BankTransferDTO bankTransferDTO
+	) {
+		OrderDTO order = orderService.placeOrder(email, cartId, paymentMethod, bankTransferDTO);
 		
 		return new ResponseEntity<OrderDTO>(order, HttpStatus.CREATED);
 	}
