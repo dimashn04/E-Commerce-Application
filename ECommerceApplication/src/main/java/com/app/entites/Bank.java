8aa08e2da1c9b7cd174fccdfddf2302412e0f391
dimashn04 <dimashn04@gmail.com>
2025-02-13 15:44:32 +0700

    Added a Bank entity for DB

---
+++ ECommerceApplication/src/main/java/com/app/entites/Bank.java
@@ -0,0 +1,26 @@
+package com.app.entites;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "banks")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    @NotBlank
+    private String storeAccountNumber;
+}
